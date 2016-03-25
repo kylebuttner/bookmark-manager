895d57627616523453adba6f150406ae87c1e9bf
Kyle Buttner <kylebuttner@gmail.com>
2016-03-25 11:56:55 +0000

    add user model spec

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -47,3 +47,11 @@ feature 'User sign in' do
   end
 
 end
+
+feature 'User sign out' do
+  let!(:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234')
+  end
+end
